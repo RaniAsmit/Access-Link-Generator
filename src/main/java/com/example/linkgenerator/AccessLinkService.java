1b393764abbe7085eed6a54cbd691c35d0af619a
Rani Asmit <raniasmit200204@gmail.com>
2025-06-04 18:10:03 +0530

    update

--- src/main/java/com/example/linkgenerator/AccessLinkService.java
+++ src/main/java/com/example/linkgenerator/AccessLinkService.java
@@ -13,8 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.linkgenerator.config.AppConfig;
-
 @Service
 public class AccessLinkService {
 
@@ -22,9 +20,6 @@ public class AccessLinkService {
 
     @Autowired
     AccessLinkRepository accessLinkRepository;
-
-    @Autowired
-    private AppConfig appConfig;
 
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm a")
             .withZone(ZoneId.of("Asia/Kolkata")); // Indian Standard Time (IST)
@@ -40,7 +35,7 @@ public class AccessLinkService {
         AccessLink accessLink = new AccessLink();
         String id = UUID.randomUUID().toString();
         accessLink.setId(id);
-        accessLink.setUrl(appConfig.getBaseUrl() + "/api/access-links/" + id);
+        accessLink.setUrl("http://localhost:8080/api/access-links/" + id);
         accessLink.setTargetResource(formattedUrl);
         accessLink.setExpiryTime(expiryTime);
         accessLink.setUsed(false);
